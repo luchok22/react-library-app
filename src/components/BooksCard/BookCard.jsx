07f305448d83c0871d3f74d82e0fcc32020c27af
luchok22 <aitbaev2021@mail.ru>
2023-04-04 20:27:57 +0600

    feat: add order category

--- src/components/BooksCard/BookCard.jsx
+++ src/components/BooksCard/BookCard.jsx
@@ -12,12 +12,12 @@ const BookCard = ({ volumeInfo }) => {
       <div className={scss.about}>
         <span>{volumeInfo.authors}</span>
         <div>{volumeInfo.title}</div>
-        <div className={scss.more}>
-          <button>Read more</button>
-        </div>
         <div className={scss.category}>
           <span>Категория:</span>
           <p>{volumeInfo.categories}</p>
+        </div>
+        <div className={scss.more}>
+          <button>Read more</button>
         </div>
       </div>
     </div>
--- src/components/SearchBooks/SearchBooks.jsx
+++ src/components/SearchBooks/SearchBooks.jsx
@@ -1,51 +1,69 @@
-import React, { useMemo } from "react";
-import { useState } from "react";
+import React, { useMemo, useState } from "react";
 import axios from "axios";
 import BookCard from "../BooksCard/BookCard";
 import scss from "./SearchBooks.module.scss";
+
+const orderBtn = ["newest", "relevance"];
+
 const SearchBooks = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [books, setBooks] = useState([]);
-  const [totalBooks, setTotalBooks] = useState(0)
-  const [visibleBooks, setVisibleBooks] = useState(10)
+  const [totalBooks, setTotalBooks] = useState(0);
+  const [visibleBooks, setVisibleBooks] = useState(10);
   const [showMore, setShowMore] = useState(false);
+  const [order, setOrder] = useState("relevance");
+
   const handleSearch = async (event) => {
     event.preventDefault();
-    const response = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&orderBy=newest&maxResults=40&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU`
-    );
+    let maxResults = order === "newest" ? 10 : 40;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&orderBy=${order}&maxResults=${maxResults}`;
+    const response = await axios.get(url);
     setBooks(response.data.items);
-    setTotalBooks(response.data.totalItems)
-    setVisibleBooks(10)
+    setTotalBooks(response.data.totalItems);
+    setVisibleBooks(8);
   };
+
   const renderBooks = useMemo(() => {
     return books.slice(0, visibleBooks).map((el) => <BookCard {...el} />);
   }, [books, visibleBooks]);
 
   const handleShowMore = () => {
-    setVisibleBooks(visibleBooks * 3)
+    setVisibleBooks(visibleBooks * 3);
     setShowMore(true);
   };
+
   const handleInputChange = () => {
     setShowMore(false);
   };
-  console.log(books);
+
   return (
     <div className={scss.search}>
-        <div className={scss.back}>
-          <h1>Search for Books</h1>
-          <form onSubmit={handleSearch}>
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(event) => { setSearchTerm(event.target.value); handleInputChange();}}
-            />
-          </form>
-        </div>
+      <div className={scss.back}>
+        <h1>Search for Books</h1>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => {
+              setSearchTerm(event.target.value);  
+              handleInputChange();
+            }}
+          />
+          <select
+            value={order}
+            onChange={(event) => {
+              setOrder(event.target.value);
+              handleInputChange();
+            }}
+          >
+           {orderBtn.map((order) => (
+              <option value={order}>{order}</option>
+            ))}
+          </select>
+        </form>
+      </div>
       <div className={scss.container}>
-        <div className={scss.total}>
-        Found {totalBooks} result
-        </div>
+        <div className={scss.total}>Found {totalBooks} results</div>
         <div className={scss.wrapper}>{renderBooks}</div>
         <div className={scss.more}>
           {!showMore && books.length > 4 && (
